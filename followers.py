aa648796fe9a362c4ed02392fe5cdd2c5e2c3a5f
Daniel Hepper <daniel.hepper@gmail.com>
2012-09-04 23:58:32 +0200

    Small script to test fetching all followers

---
+++ followers.py
@@ -0,0 +1,19 @@
+import sys
+from libsaas.services import github
+
+def get_all_followers(service, username):
+    followers = []
+    empty = False
+    page = 0
+    while not empty:
+        current_page = gh.user(name=username).followers(page=page)
+        followers.extend(current_page)
+        empty = len(current_page)==0
+        page += 1
+
+if __name__  == '__main__':
+    if len(sys.argv) != 4:
+        print "Usage: %s <your_username> <password> <other_username>"%sys.argv[0]
+        sys.exit(-1)
+    gh = github.GitHub(sys.argv[1], sys.argv[2])
+    print len(get_all_followers(gh, sys.argv[3]))
